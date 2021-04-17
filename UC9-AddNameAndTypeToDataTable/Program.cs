b0b5a454962aa5565899c88b2769dad386659d13
Mansi-Atey <mansiatey31@gmail.com>
2021-04-17 20:54:15 +0530

    [mansi]Added: ability to add name and type to datatable

---
+++ UC9-AddNameAndTypeToDataTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+namespace UC9_AddNameAndTypeToDataTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System using Linq and DataTable");
+            AddressBookDataTable addressBookDataTable = new AddressBookDataTable();
+            DataTable table = addressBookDataTable.CreateAddressBookDataTable();
+            addressBookDataTable.DisplayContacts(table);
+            //addressBookDataTable.EditContact(table);
+            //addressBookDataTable.DeleteContact(table);
+            //addressBookDataTable.RetrieveContactBelonginhToPerticularCityORState(table);
+            //addressBookDataTable.CountContactsFromPerticularCityANDState(table);
+            //addressBookDataTable.SortContacts(table);
+        }
+    }
+}
