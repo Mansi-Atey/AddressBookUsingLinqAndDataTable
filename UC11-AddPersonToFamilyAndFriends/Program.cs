1eb8412c580f7815a06f4a036dd82dceb8c924ab
Mansi-Atey <mansiatey31@gmail.com>
2021-04-17 21:15:07 +0530

    [mansi]Added: ability to add person in both friends and family

---
+++ UC11-AddPersonToFamilyAndFriends/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+
+namespace UC11_AddPersonToFamilyAndFriends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System using Linq and DataTable");
+            AddressBookDataTable addressBookDataTable = new AddressBookDataTable();
+            DataTable table = addressBookDataTable.CreateAddressBookDataTable();
+            //addressBookDataTable.DisplayContacts(table);
+            //addressBookDataTable.EditContact(table);
+           // addressBookDataTable.DeleteContact(table);
+          //  addressBookDataTable.RetrieveContactBelonginhToPerticularCityORState(table);
+            //addressBookDataTable.CountContactsFromPerticularCityANDState(table);
+          //  addressBookDataTable.SortContacts(table);
+            //addressBookDataTable.CountContactsByAddressBookType(table);
+            addressBookDataTable.AddPersonInTwoTypes(table);
+        }
+    }
+}
