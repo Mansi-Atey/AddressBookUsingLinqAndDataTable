f8862a25c4977bf5916af3cc9a63d6409c9f49c8
Mansi-Atey <mansiatey31@gmail.com>
2021-04-17 19:09:59 +0530

    [mansi]Added: ability to create datatable

---
+++ UC1-CreateDataTable/AddressBookDataTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace UC1_CreateDataTable
+{
+   public class AddressBookDataTable
+    {
+        DataTable addressBookTable = new DataTable();
+    }
+}
---
+++ UC1-CreateDataTable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UC1_CreateDataTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************Welcome to Address Book System using Linq and DataTable***************");
+        }
+    }
+}
