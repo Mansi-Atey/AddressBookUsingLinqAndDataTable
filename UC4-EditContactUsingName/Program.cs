3d024aaffefe4b4efa93a44290a8fd9d3c10d4c9
Mansi-Atey <mansiatey31@gmail.com>
2021-04-17 19:46:11 +0530

    [mansi]Added: ability to edit contact using name

---
+++ UC4-EditContactUsingName/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace UC4_EditContactUsingName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System using Linq and DataTable");
+            AddressBookDataTable addressBookDataTable = new AddressBookDataTable();
+            DataTable table = addressBookDataTable.CreateAddressBookDataTable();
+            addressBookDataTable.DisplayContacts(table);
+            addressBookDataTable.EditContact(table);
+            Console.WriteLine("\n**************************************************");
+        }
+    }
+}
