d984ef5367fec895d2136ee5dac55b5e56d605c3
Mansi-Atey <mansiatey31@gmail.com>
2021-04-17 19:14:20 +0530

    [mansi]Added: ability to add attributes to datatable

---
+++ UC2-AddAttributesToDataTable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UC2_AddAttributesToDataTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********Welcome to Address Book System using Linq and DataTable***********");
+        }
+    }
+}
